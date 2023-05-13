59a9346f5d03648922934d52b9ab0d79c3e0fecd
tcminh1108 <tcminh1108@gmail.com>
2023-05-13 09:07:17 +0700

    Add entity: Overtimelog, LeaveLog, Experience

---
+++ src/Domain/Entities/Experience.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogOT.Domain.Entities;
+public class Experience
+{
+    public string NameProject { get; set; }
+    public int TeamSize { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string TechStack { get; set; }
+    public string Status { get; set; }
+    public bool IsDeleted { get; set; }
+}
