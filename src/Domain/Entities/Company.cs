f2c64c0e46256439c149b67816f410c6af07e346
Lewis <tekato2002@gmail.com>
2023-05-13 11:25:39 +0700

    Add entities: Company, Holiday, InterviewProcess, JobDescription

---
+++ src/Domain/Entities/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogOT.Domain.Entities;
+public class Company
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string AccountEmail { get; set; }
+    public string Phone { get; set; }
+    public string HREmail { get; set; }
+
+    // Relationship
+    public virtual ICollection<Holiday> Holidays { get; set; }
+    public virtual ICollection<JobDescription> JobDescriptions { get; set; }
+}
